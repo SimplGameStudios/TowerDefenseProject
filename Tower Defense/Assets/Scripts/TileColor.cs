91c9242d7817e36549b055476a7065120aa3fb0d
SebbeA03 <sebastianandersson8@gmail.com>
2020-11-07 00:54:48 +0100

    Tiles

---
+++ Tower Defense/Assets/Scripts/TileColor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileColor : MonoBehaviour
+{
+    [SerializeField] Material[] materials;
+    [SerializeField] Material equipedMaterial;
+    
+    void Start()
+    {
+        if(tag == "Spawn Tile")
+        {
+            equipedMaterial = materials[0];
+        }
+        else if(tag == "End Tile")
+        {
+            equipedMaterial = materials[1];
+        }
+        else if (tag == "Path Tile")
+        {
+            equipedMaterial = materials[2];
+        }
+        else if (tag == "Tower Tile")
+        {
+            equipedMaterial = materials[3];
+        }
+        else if (tag == "Empty Tile")
+        {
+            equipedMaterial = materials[4];
+        }
+
+        GetComponent<Renderer>().material = equipedMaterial;
+    }
+
+    void Update()
+    {
+        
+    }
+}
